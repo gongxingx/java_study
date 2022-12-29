0bb1876a69d66f7ce17e7d46a89b00cd8f94d51d
gongxingx <grin_star@163.com>
2022-12-29 23:29:21 +0800

    注释

--- 02_idea/src/HelloWorld.java
+++ 02_idea/src/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        //hello world
+        /**
+         * 注释
+         * 注释
+         * 注释
+         * 多行注释
+         */
         System.out.println("hello world, hello idea.");
     }
 }
