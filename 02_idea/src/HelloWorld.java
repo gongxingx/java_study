cbeb76dfa3b00b2da9f96399a39b4198cbb91134
gongxingx <grin_star@163.com>
2022-12-29 23:54:46 +0800

    数据类型

--- 02_idea/src/HelloWorld.java
+++ 02_idea/src/HelloWorld.java
@@ -8,5 +8,27 @@ public class HelloWorld {
          * 多行注释
          */
         System.out.println("hello world, hello idea.");
+//        数据类型
+//        基本类型
+//            整数 byte 1 short 2 int 4 long 8
+//            浮点 float 4 double 8
+//            字符类型 char 2
+//            boolean 1位 true false
+//        引用类型 类 接口 数组
+        int a = 10;
+        byte b = 20;
+        short c = 30;
+        long d = 30L;
+        float e = 2.1f;
+        double f = 3.14;
+        char g = 'a';
+//        String 是类
+        boolean h = false;
+        Byte bb;
+
+//        进制
+        int i = 10;
+        int i2 = 010;
+        int i3 = 0x10;
     }
 }
